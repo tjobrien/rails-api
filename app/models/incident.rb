db45b495e2ce0449e18dfe95c923d5c6b6948bb1
Thomas OBrien <tobrien999@gmail.com>
2021-03-17 10:35:20 -0700

    updates for testing

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,3 +1,16 @@
 class Incident < ApplicationRecord
   enum payment_mode: [:null, :prepaid, :insurance, :invoice, :partnership, :employeebenefit]
+
+  #after_update_commit :patch_on_ready_core
+  after_save :patch_on_ready_core
+
+  private
+  def patch_on_ready_core
+    if saved_change_to_name?
+      logger.debug( "name changed");
+    else
+      logger.debug("Name not changed");
+    end
+  end
+
 end
